b5dc9667124ea661535dfd5f7b64948af5026e31
Code-Rooster <codingroosters@gmail.com>
2022-07-21 15:41:52 -0700

    0.0.20

--- src/classes/camera.js
+++ src/classes/camera.js
@@ -56,33 +56,42 @@ export class camera {
      * @param {vector3} rotatedPos 
      */
     worldToCamPoint(worldPos) {
-        let rotatedPos = quaternion.multiply(this.rot, quaternion.quatFromPoint(worldPos), this.rot.inverse);
+        let rotatedPos = quaternion.multiply(this.rot.inverse, quaternion.quatFromPoint(worldPos));
+        rotatedPos = quaternion.multiply(rotatedPos, this.rot);
+
+        let firstPerpendicularVecRot = quaternion.unitQuatFromAngleAxis(Math.PI / 2, new vector3(1, 0, 0));
+        let secondPerpendicularVecRot = quaternion.unitQuatFromAngleAxis(Math.PI / 2, new vector3(0, 1, 0));
+        let firstPerpendicularVec = quaternion.multiply(firstPerpendicularVecRot, this.rot);
+        firstPerpendicularVec = quaternion.multiply(firstPerpendicularVec, firstPerpendicularVecRot.inverse).normalized();
+        let secondPerpendicularVec = quaternion.multiply(secondPerpendicularVecRot, this.rot);
+        secondPerpendicularVec = quaternion.multiply(secondPerpendicularVec, secondPerpendicularVecRot.inverse).normalized();
+
+        let firstPerpendicularAxis = firstPerpendicularVec.axis;
+        let secondPerpendicularAxis = secondPerpendicularVec.axis;
 
         let camAxis = this.rot.axis;
-
-        console.log(this.rot.angle);
-        console.log(this.rot.axis);
 
         let canvasWidth = parseFloat(this.canvas.width);
         let canvasHeight = parseFloat(this.canvas.height);
 
-        let offsetWorldPosMagnitude = Math.sqrt(Math.pow(rotatedPos.x - this.pos.x, 2), Math.pow(rotatedPos.y - this.pos.y, 2), Math.pow(rotatedPos.z - this.pos.z, 2));
+        let posDelta = vector3.subtract(rotatedPos, this.pos);
 
-        let theta = - Math.acos(vector3.dotProduct(camAxis, vector3.subtract(rotatedPos, this.pos)) / (camAxis.magnitude * vector3.subtract(rotatedPos, this.pos).magnitude));
-        let alpha = - Math.acos(vector3.dotProduct(camAxis.firstPerpendicularVec, vector3.subtract(rotatedPos, this.pos).normalized()) / (camAxis.firstPerpendicularVec.magnitude * offsetWorldPosMagnitude));
-        let gamma = - Math.acos(vector3.dotProduct(camAxis.secondPerpendicularVec, vector3.subtract(rotatedPos, this.pos).normalized()) / (camAxis.secondPerpendicularVec.magnitude * offsetWorldPosMagnitude));
+        let theta = - Math.acos(vector3.dotProduct(camAxis, posDelta) / (camAxis.magnitude * posDelta.magnitude));
+        let alpha = - Math.acos(vector3.dotProduct(firstPerpendicularAxis, posDelta) / (firstPerpendicularAxis.magnitude * posDelta.magnitude));
+        let gamma = - Math.acos(vector3.dotProduct(secondPerpendicularAxis, posDelta) / (secondPerpendicularAxis.magnitude * posDelta.magnitude));
 
         let h = Math.sqrt(Math.pow(rotatedPos.x - this.pos.x, 2) + Math.pow(rotatedPos.y - this.pos.y, 2) + Math.pow(rotatedPos.z - this.pos.z, 2));
 
         let thetaLength = h * Math.cos(theta);
-        let alphaLength = h * Math.sin(alpha);
-        let gammaLength = h * Math.sin(gamma);
+        let alphaLength = h * Math.cos(alpha);
+        let gammaLength = h * Math.cos(gamma);
 
         if(true) {
             let screenPos = { 
-                x: canvasWidth * (alphaLength / (thetaLength) + 0.5),
-                y: canvasHeight * (gammaLength / (thetaLength) + 0.5),
+                x: canvasWidth * (1 - ((alphaLength / (thetaLength) + 0.5))),
+                y: canvasHeight * (1 - ((gammaLength / (thetaLength) + 0.5))),
                 lineThickness: (1 / thetaLength) * ((canvasWidth + canvasHeight) / 100) };
+
             return screenPos;
         } else {
             return { x: 0, y: 0, lineThickness: 0 };
